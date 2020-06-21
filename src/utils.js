b65ac14729106165597c6a48a581e535eee665d0
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-06-21 12:42:09 +0200

    create 2d array

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,21 @@
-const config = {};
-config.controller = 1; // player
-config.map_width = 4;
-config.map_height = 4;
-export default config;
+// function to create 2d Array
+// pass parameters
+function create2dArray() {
+  let a = new Array(4);
+  let sOut = '<table border=2 class="table"> ';
+  for (let i = 0; i < 4; i++) {
+    // for each row
+    sOut += '<tr class="trow"">';
+    a[i] = new Array(4);
+    for (let j = 0; j < 4; j++) {
+      a[i][j] = '' + j + ', ' + i + '';
+      // for each clm
+      sOut += '<td class="tdata">' + a[i][j] + '</td>';
+    }
+    sOut += '</tr>';
+  }
+  sOut += '</table>';
+  return a;
+}
+
+module.exports = { create2dArray };
